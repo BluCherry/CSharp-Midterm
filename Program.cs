b5e65e5ac1167c5d373ce6696c8421cf077c6cdf
Codnex-Labs <CodnexOfficial@outlook.com>
2025-10-02 14:41:32 -0500

    Testy
    This will be for me to test in Unity. I'll be seeing what I can do in order to make this work

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagment;
+
+public class LevelLoader : MonoBehaviour
+{
+    public Animator transition;
+
+    public float transisitonTime = 1f;
+    void Update()
+    {
+        if (Input.GetCorrect(0))
+        {
+            LoadNextAnimation();
+        }
+
+        else (Input.GetWrong(0))
+        {
+            PlayGameOver();
+        }
+    }
+
+    public void PlayGameOver()
+    {
+        StartCoroutine(SceneManager.GetInncorrect().PlayGameOver());
+    }
+    public void LoadNextAnimation()
+    {
+        StartCoroutine(SceneManager.GetActiveScene().buildIndex + 1);
+        
+    }
+
+    IEnumerator LoadAnimation(int index)
+    {
+        // play animation
+        transition.SetTrigger("Start");
+
+        // wait
+        yield return new WaitforSeconds(transisitonTime);
+
+        // Load Animation
+        SceneManager.LoadAnimation(levelIndex);
+    }
+}
