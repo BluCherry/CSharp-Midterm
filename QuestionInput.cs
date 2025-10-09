1670e4ea19d88bdd3d75fa11ba524d59746393a2
BluCherry <cgroves317@gmail.com>
2025-10-09 16:37:46 -0500

    Unity-compliant files upload

---
+++ QuestionInput.cs
@@ -0,0 +1,31 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuestionInput : MonoBehaviour
+{
+    public static string userAnswer;
+    public TMP_InputField inputField;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void ReadStringInput(string input)
+    {
+        userAnswer = input;
+        Debug.Log(userAnswer);
+        inputField.text = ""; // Clears the input field
+        // Get a reference to the GameObject
+        GameObject gameObject = GameObject.Find("QuestionOutputScript");
+
+        // Send the message
+        gameObject.SendMessage("CheckAnswer");
+    }
+}
