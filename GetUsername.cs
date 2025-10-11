18ebf8beb28bcf5c21c6242170a594c4404f6075
BluCherry <cgroves317@gmail.com>
2025-10-11 14:18:18 -0500

    Update GetUsername.cs
    Remove unused functions and add input validation

--- GetUsername.cs
+++ GetUsername.cs
@@ -4,21 +4,15 @@ public class GetUsername : MonoBehaviour
 {
     public static string username;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public static void ReadStringInput(string input)
     {
+        if (input == null || input == "")
+        {
+            throw new MissingComponentException("username cannot be null or empty");
+        }
+
         username = input;
         Debug.Log(username);
     }
 }
+
